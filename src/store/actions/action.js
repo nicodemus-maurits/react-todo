8b85b83f2ab973883ac57ec51d514aa97cb4c9b4
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-10-10 13:33:42 +0200

    Create action constant

---
+++ src/store/actions/action.js
@@ -0,0 +1,7 @@
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+
+export const ADD_CATEGORY = 'ADD_CATEGORY';
+export const REMOVE_CATEGORY = 'REMOVE_CATEGORY';
